52a3319ff5aab1dd1fbfd0f096981654afffdf6b
deeptanshu01 <87687372+deeptanshu01@users.noreply.github.com>
2022-06-23 08:53:02 +0530

    Add files via upload

---
+++ create_dictionary.m
@@ -0,0 +1,34 @@
+
+allwords = "";
+
+% - Load and concatenate all the emails in one string
+Files=dir('data/*/*.txt');
+for k=1:length(Files)
+    FileNames = Files(k).name;
+    dr = Files(k).folder;
+    file = fileread(strcat(dr, '\', FileNames));
+    file = regexprep(file, '\<\w{1,2}\>', "");
+    allwords = strcat(allwords, ' ', file);
+end
+
+% - Split the emails by white space so we can count the number of occurence of each of the words
+words = strsplit(allwords, ' ' )';
+[words_u, ~, idxU] = unique(words);
+counts = accumarray( idxU, 1 );
+% - Sort entries by count.
+[~, idxS] = sort( counts, 'descend' );
+words_us = words_(1:2500);
+counts_s = counts(1:2500);
+
+% - Build cell array of unique words and counts.
+result = [words_us, num2cell( counts_s )];
+result = result(1:2500, :);
+
+% - Save the word count in dictionary.txt file
+filePh = fopen('dictionary.txt','w');
+[rows,cols]=size(result);
+for r=1:rows
+    fprintf(filePh,'%d. %s %d\n', r, result{:}, result{1});
+end
+fclose(filePh);
+view rawcreate_dictionary.m 
